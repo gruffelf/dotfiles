4916cda96f22c894c870154c44782caff10614ee
Aiden Torpy <aiden.torps@gmail.com>
2025-04-23 22:10:05 +1000

    Added clipboard commands

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,7 @@
 local vim = vim
 local Plug = vim.fn['plug#']
 
+-- vim-plug init
 vim.call('plug#begin')
 
 Plug('tpope/vim-sensible')
@@ -8,12 +9,19 @@ Plug('olimorris/onedarkpro.nvim')
 
 vim.call('plug#end')
 
+-- Add line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+-- Fix tab indentation width
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 
+-- Global Clipboard Commands gy, gp
+vim.keymap.set({'n', 'x', 'o'}, 'gy', '"+y', {desc = 'Copy to clipboard'})
+vim.keymap.set({'n', 'x', 'o'}, 'gp', '"+p', {desc = 'Paste clipboard text'})
+
+-- onedarkpro theme
 require("onedarkpro").setup({
 	options = {
 		transparency = true,
